0328b3433884f2dda8726485c54c299ae2f915cc
TANMAYBAGADIA <tanmaybagadia1301@gmail.com>
2021-06-26 09:52:48 +0530

    ALl done

---
+++ src/BlogDetails.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useParams } from "react-router";
+import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch(
+    "http://localhost:8000/blogs/" + id
+  );
+  const history = useHistory();
+  const [body, setBody] = useState("");
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const comment = { body };
+
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(comment),
+    }).then(() => {
+      // history.go(-1);
+      history.push("/");
+    });
+  };
+  return (
+    <div className="blog-details" onSubmit={handleSubmit}>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
+          
+          
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
